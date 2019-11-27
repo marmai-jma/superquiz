506746221e637097695659a24bec7d4322a1526c
marmai-jma <johane.marmai@gmail.com>
2019-11-27 15:59:33 +0100

    Refactor la page séparer en navbar home footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarComponent, FooterComponent, HomeComponent ],
   imports: [ BrowserModule ],
   bootstrap: [AppComponent]
 })
