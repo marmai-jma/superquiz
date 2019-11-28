7f129c32ebc2168073648d8c070a6d2d313a0d08
marmai-jma <johane.marmai@gmail.com>
2019-11-28 15:58:02 +0100

    Todo avec add et toggle dans la liste

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,11 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Todo {
+  text: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[] = [
+    {text: 'Mon premier todo', done: false},
+    {text: 'Préparer la formation', done: true},
+    {text: 'Revoir les bases de JavaScript', done: false},
+    {text: 'Passer son permis de chasse', done: false},
+    {text: `Because I'm Batman`, done: true},
+  ];
+
+  addTodo(newText: string, event: Event) {
+    // empecher la soumission du formulaire (evenement par défaut)
+    event.preventDefault();
+    this.todos.push({text: newText , done: false});  // pas de new Todo car interface, pas classe
+  }
+
+  toggleTodo(todo: Todo){
+    // Quick and dirty - on attaque directement l'occurence. (effets de bord)
+    // todo.done = ! todo.done;
+
+    // immutabilité - renvoyer tout le tableau
+    // On voudrait recréer la liste avec chaque todo à sa bonne valeur
+    // On change le done pour le todo de la liste dont le texte correspond au texte du todo en param
+      this.todos = this.todos.map(td => {
+      return td.text === todo.text ? {...td, done : !td.done} : td;
+    });
+  }
+
+  deleteTodo() {}
 
   constructor() { }
 
